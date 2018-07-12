b488dc3cc9b00996ab5f2a593e61b878f63c0846
ahmed-fatouh <39876806+ahmed-fatouh@users.noreply.github.com>
2018-07-12 16:08:54 +0200

    Add Error Controller

---
+++ CoreBB.Web/Controllers/ErrorController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Hosting;
+
+namespace CoreBB.Web.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public ViewResult Index()
+        {
+            var exception = HttpContext.Features.Get<IExceptionHandlerFeature>();
+            ViewData["StatusCode"] = HttpContext.Response.StatusCode;
+            ViewData["Message"] = exception.Error.Message;
+            ViewData["StackTrace"] = exception.Error.StackTrace;
+            ViewBag.Title = "Error Page";
+            return View();
+        }
+
+        public ViewResult AccessDenied()
+        {
+            ViewBag.Title = "Access Denied";
+            return View();
+        }
+    }
+}
