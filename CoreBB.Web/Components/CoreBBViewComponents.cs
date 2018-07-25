eaea1029a5043d3fc9118e399ff0b73ebbf9f5eb
ahmed-fatouh <39876806+ahmed-fatouh@users.noreply.github.com>
2018-07-25 20:24:41 +0200

    Add Components used by Views

---
+++ CoreBB.Web/Components/CoreBBViewComponents.cs
@@ -0,0 +1,39 @@
+﻿using CoreBB.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreBB.Web.Components
+{
+    public class ReplyViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(Topic model) => View(model);
+    }
+
+    public class EditViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(Topic model) => View(model);
+    }
+
+    public class DeleteViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(Topic model) => View(model);
+    }
+
+    public class DeleteUserViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(User model) => View(model);
+    }
+
+    public class DeleteForumViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(Forum model) => View(model);
+    }
+
+    public class DeleteMessageViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(Message model) => View(model);
+    }
+}
